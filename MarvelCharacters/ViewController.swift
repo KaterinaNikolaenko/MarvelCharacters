72d2f623fb1df4dfca6fc41ff70eb5a6b229db98
Katerina Nikolaenko <katya-nik@list.ru>
2021-06-13 21:50:56 +0200

    Initial Commit

---
+++ MarvelCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Katerina Nikolaenko on 13.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
